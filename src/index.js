1d58b76514eec2a02c6fc540e0d88db0eea01356
jcliang <dachurita@gmail.com>
2021-08-29 20:58:18 +0800

    0829

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { chain } = require('bottender');
 async function DefaultAction(context) {
   await context.sendText('輸入\"help\"會顯示功能選單。\n目前只有\'課程資訊查詢\'有回應。');
 }
+
 //顯示Inline kyeboard 的 function
 async function ShowInlineKB(context) {
   //keyboard寫進一個變數裡
@@ -71,12 +72,19 @@ async function ShowKB(context) {
   };
   await context.sendText('Keyboard 出來吧！', { replyMarkup });
 };
-
+async function showSyntaxHint(context) {
+  await context.sendText('以＄開頭，後面輸入想要查詢的語法\n ex:$print');
+}
+async function showSyntax(context) {
+  await context.sendPhoto('https://i.imgur.com/r5PdC4o.png');
+}
 function RuleBased(context, {next}){
   return router([
     text('課程資訊查詢', ShowInlineKB),
+    text('語法解說', showSyntaxHint),
     text('help', ShowKB),
     // return the `next` action
+    text(/^\$/, showSyntax),
     route('*', next),
   ]);
 }
