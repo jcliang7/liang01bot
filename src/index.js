02f261500ca602c3c011352987b90b4cbf769f79
jcliang <jcliang@cycu.org.tw>
2021-10-10 13:16:57 +0800

    1010

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-// module.exports = require('./base64');//encode text to base64 string
-module.exports = require('./csbotindex');
+module.exports = require('./base64');//encode text to base64 string
+// module.exports = require('./csbotindex');
