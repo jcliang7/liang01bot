d55d09e0ad8182f0000086f11c03f5449c4deb21
Jia-Chi Liang <jcliang@cycu.org.tw>
2021-10-26 17:53:00 +0800

    answer add unit  button

--- src/callback.js
+++ src/callback.js
@@ -74,15 +74,11 @@ async function callBackCallKeyboard(context) {
   const replyMarkup = {
     myStr:"請選擇，也可以直接輸入「題目名稱」或 「關鍵字」:",
     keyboard: [
-      [{ text: '基本輸出' }, { text: '印出字串' }, { text: '字串輸出' }],
-
-      [{ text: '長方體體積' }, { text: '大寫換小寫' }, { text: '圓柱表面積' }, { text: '整數除法' }],
-
-      [{ text: '三數排序' }, { text: '四數排序' }, { text: '判斷及調整成績' }, { text: '判斷月份' }],
-      
-      [{ text: '數字平方和' }, { text:'求數字和' }, { text:'質數判別' }, { text:'九九乘法表(單排)' }],
-            // [{text:'btn'},{text:'btn'}, {text:'btn'}, {text:'btn'}],
+      [{ text: '求兩數和 (N次)'}, { text: '求兩數和 (非零)' }],
+      [{ text: '求兩數和 (檔尾)' }, { text: '求兩個數的最大公因數' }],
+      [{ text: '求位數和' }, { text: ' ' }],
       // [{text:'btn'},{text:'btn'}, {text:'btn'}, {text:'btn'}],
+      [{text: '作業參考答案'}],
       [{text: '回主選單'}],
     ],
     resizeKeyboard: true,
